b3f8e680dbceba2f8cffc3bc7de8b437cf74914f
Adam Ouellette <adamouellette57@gmail.com>
2015-10-14 23:27:15 -0400

    removed all user functionality

--- db/schema.rb
+++ db/schema.rb
@@ -13,21 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151012185245) do
 
-  create_table "admins", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
-    t.integer  "sign_in_count",      default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",    default: 0
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "full_time_positions", force: true do |t|
     t.string   "position"
     t.string   "company"
