b7d4e6740646ab5685a84d2b33b573f2dc2303ea
Adam Ouellette <adamouellette57@gmail.com>
2015-11-03 21:35:32 -0500

    added controllers fr internship/full time posiiton

--- app/controllers/full_time_positions_controller.rb
+++ app/controllers/full_time_positions_controller.rb
@@ -1,4 +1,89 @@
-class FullTimePositionsController < InheritedResources::Base
+class FullTimePositionsController < ApplicationController
+
+  def index
+  	@full_time_positions = FullTimePosition.all
+
+  	respond_to do |format|
+  	  format.html # index.html.erb
+  	  format.json { render json: @full_time_positions }
+  	end
+  end
+
+  # GET /full_time_position/1
+  # GET /full_time_position/1.json
+  def show
+  	@full_time_position = FullTimePosition.find(params[:id])
+
+  	respond_to do |format|
+  		format.html # show.html.erb
+  		format.json { render json: @full_time_position }
+  	end
+  end
+
+  # GET /full_time_position/new
+  # GET /full_time_position/new.json
+  def new
+  	@full_time_position = FullTimePosition.new
+
+  	respond_to do |format|
+  		format.html #new.html.erb
+  		format.json { render json: @full_time_position }
+  	end
+  end
+
+  # GET /full_time_position/1/edit
+  def edit
+  	@full_time_position = FullTimePosition.find(params[:id])
+
+  	respond_to do |format|
+  		format.html #show.html.erb
+  		format.json { render json: @full_time_position }
+  	end
+  end
+
+  # POST /full_time_position
+  # POST /full_time_position.json
+  def create
+  	@full_time_position = FullTimePosition.new(full_time_position_params)
+
+  	respond_to do |format|
+  		if full_time_position.save
+  			format.html { redirect_to @full_time_position, notice: 'Position was successfully created'}
+  			format.json { render json: @full_time_position, status: :created, location: @full_time_position }
+  		else
+  			format.html { render action: "new" }
+  			format.json { render json: @full_time_position.errors, status: :unprocessable_entity }
+  		end
+  	end
+  end
+
+  # PUT /full_time_position/1
+  # PUT /full_time_position/1.json
+  def update
+  	@full_time_position = FullTimePosition.find(params[:id])
+
+  	respond_to do |format|
+  		if @full_time_position.update_attributes(full_time_position_params)
+  			format.html { redirect_to @full_time_position, notice: 'Posiiton was successfully updated.' }
+  			format.json { head :no_content }
+  		else
+  			format.html { render action: "edit" }
+  			format.json { render json: @full_time_position.errors, status: :unprocessable_entity }
+  		end
+  	end
+  end
+
+  # DELETE /full_time_position/1
+  # DELETE /full_time_position/1.json
+  def destroy
+  	@full_time_position = FullTimePosition.find(params[:id])
+  	@full_time_position.destroy
+
+  	respond_to do |format|
+  		format.html { redirect_to full_time_position_url }
+  		format.json { head :no_content }
+  	end
+  end
 
   private
 
--- app/controllers/internship_positions_controller.rb
+++ app/controllers/internship_positions_controller.rb
@@ -1,4 +1,89 @@
-class InternshipPositionsController < InheritedResources::Base
+class InternshipPositionsController < ApplicationController
+
+  def index
+  	@internship_positions = InternshipPosition.all
+
+  	respond_to do |format|
+  	  format.html # index.html.erb
+  	  format.json { render json: @internship_time_positions }
+  	end
+  end
+
+  # GET /internship_position/1
+  # GET /internship_position/1.json
+  def show
+  	@internship_position = InternshipPosition.find(params[:id])
+
+  	respond_to do |format|
+  		format.html # show.html.erb
+  		format.json { render json: @internship_position }
+  	end
+  end
+
+  # GET /internship_position/new
+  # GET /internship_position/new.json
+  def new
+  	@internship_position = InternshipPosition.new
+
+  	respond_to do |format|
+  		format.html #new.html.erb
+  		format.json { render json: @internship_position }
+  	end
+  end
+
+  # GET /internship_position/1/edit
+  def edit
+  	@internship_position = InternshipPosition.find(params[:id])
+
+  	respond_to do |format|
+  		format.html #show.html.erb
+  		format.json { render json: @internship_position }
+  	end
+  end
+
+  # POST /internship_position
+  # POST /internship_position.json
+  def create
+  	@internship_position = InternshipPosition.new(full_time_position_params)
+
+  	respond_to do |format|
+  		if internship_position.save
+  			format.html { redirect_to @internship_position, notice: 'Position was successfully created'}
+  			format.json { render json: @internship_position, status: :created, location: @internship_position }
+  		else
+  			format.html { render action: "new" }
+  			format.json { render json: @internship_position.errors, status: :unprocessable_entity }
+  		end
+  	end
+  end
+
+  # PUT /internship_position/1
+  # PUT /internship_position/1.json
+  def update
+  	@internship_position = InternshipPosition.find(params[:id])
+
+  	respond_to do |format|
+  		if @internship_position.update_attributes(full_time_position_params)
+  			format.html { redirect_to @internship_position, notice: 'Posiiton was successfully updated.' }
+  			format.json { head :no_content }
+  		else
+  			format.html { render action: "edit" }
+  			format.json { render json: @internship_position.errors, status: :unprocessable_entity }
+  		end
+  	end
+  end
+
+  # DELETE /internship_position/1
+  # DELETE /internship_position/1.json
+  def destroy
+  	@internship_position = InternshipPosition.find(params[:id])
+  	@internship_position.destroy
+
+  	respond_to do |format|
+  		format.html { redirect_to full_time_position_url }
+  		format.json { head :no_content }
+  	end
+  end
 
   private
 
